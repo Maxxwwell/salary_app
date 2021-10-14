49e76fc745c130fa0fb799da770315065df8d3d7
unknown <agyeimaxwell54@gmail.com>
2021-10-14 18:48:38 +0000

    minor changes

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -30,167 +30,163 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        drawer: const Drawer(),
-        appBar: AppBar(
-          backgroundColor: Colors.grey[50],
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Theme.of(context).primaryColor,
-          ),
-          actions: const [
-            PopupIcon(),
-          ],
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      drawer: const Drawer(),
+      appBar: AppBar(
+        backgroundColor: Colors.grey[50],
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
         ),
-        body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-          height: MediaQuery.of(context).size.height * 0.88,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Know Your Salary, Tax and Allowances',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                ),
+        actions: const [
+          PopupIcon(),
+        ],
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        height: MediaQuery.of(context).size.height * 0.88,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Know Your Salary, Tax and Allowances',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
               ),
-              const SizedBox(height: 15),
-              TextField(
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400,
-                ),
-                keyboardType:
-                    const TextInputType.numberWithOptions(decimal: true),
-                onChanged: (value) {
-                  convertSalary(value);
+            ),
+            const SizedBox(height: 15),
+            TextField(
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w400,
+              ),
+              keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),
+              onChanged: (value) {
+                convertSalary(value);
 
-                  print(value);
-                },
-                decoration: textFieldDecoration(context),
-              ),
-              const SizedBox(height: 25),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.16,
-                width: double.infinity,
-                child: Card(
-                  elevation: 5,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Title_Image(
-                        image: 'assets/icons/wallet.png',
-                        title: 'Take Home Amount',
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(
-                            horizontal:
-                                MediaQuery.of(context).size.width * 0.2),
-                        child: Text(
-                          'GH₵ ' + _takeHome,
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w400,
-                          ),
+                print(value);
+              },
+              decoration: textFieldDecoration(context),
+            ),
+            const SizedBox(height: 25),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.16,
+              width: double.infinity,
+              child: Card(
+                elevation: 5,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Title_Image(
+                      image: 'assets/icons/wallet.png',
+                      title: 'Take Home Amount',
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: MediaQuery.of(context).size.width * 0.2),
+                      child: Text(
+                        'GH₵ ' + _takeHome,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 15),
-              Wrap(
-                spacing: 10,
-                runSpacing: 15,
-                children: [
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.16,
-                    width: MediaQuery.of(context).size.width * 0.46,
-                    child: Card(
-                      elevation: 5,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Title_Image(
-                            image: 'assets/icons/tax.png',
-                            title: 'PAYE Tax',
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Text(
-                              'GH₵ ' + _tax,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400,
-                              ),
+            ),
+            const SizedBox(height: 15),
+            Wrap(
+              spacing: 10,
+              runSpacing: 15,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.16,
+                  width: MediaQuery.of(context).size.width * 0.46,
+                  child: Card(
+                    elevation: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Title_Image(
+                          image: 'assets/icons/tax.png',
+                          title: 'PAYE Tax',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            'GH₵ ' + _tax,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.16,
-                    width: MediaQuery.of(context).size.width * 0.46,
-                    child: Card(
-                      elevation: 5,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Title_Image(
-                            image: 'assets/icons/money.png',
-                            title: 'Pension +',
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: Text(
-                              'GH₵ ' + _pension,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w400,
-                              ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.16,
+                  width: MediaQuery.of(context).size.width * 0.46,
+                  child: Card(
+                    elevation: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Title_Image(
+                          image: 'assets/icons/money.png',
+                          title: 'Pension +',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Text(
+                            'GH₵ ' + _pension,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w400,
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.16,
-                    width: double.infinity,
-                    child: Card(
-                      elevation: 5,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Title_Image(
-                            image: 'assets/icons/vault.png',
-                            title: 'Employer Pension Amount',
-                          ),
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal:
-                                    MediaQuery.of(context).size.width * 0.2),
-                            child: Text(
-                              'GH₵ ' + _employerPension,
-                              style: const TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w400,
-                              ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.16,
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 5,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Title_Image(
+                          image: 'assets/icons/vault.png',
+                          title: 'Employer Pension Amount',
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal:
+                                  MediaQuery.of(context).size.width * 0.2),
+                          child: Text(
+                            'GH₵ ' + _employerPension,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w400,
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
